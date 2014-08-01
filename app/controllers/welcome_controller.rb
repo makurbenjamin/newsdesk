7e90da34c20e6017c36b4b6c857aca480eddc22c
makurbenjamin <makurbenjamin@gmail.com>
2014-08-01 13:44:07 +0200

    add menu

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-  	@pieces = Piece.all
+  	@pieces = Piece.limit(3)
+  	@pieces2 = Piece.limit(2).offset(15)
   	@opinions = Opinion.all
+  	@pic = Piece.all
   end
 end
